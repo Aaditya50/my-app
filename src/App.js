9466a945f8f919e44b58bf8e548956fddbdadc4f
Aaditya55 <aadityajoshi291020@gmail.com>
2022-12-20 22:29:01 +0530

    hello_react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return ( 
-        <div> 
-          <h2>
-            hello 
-          </h2> 
-        
-          <h1>hello React</h1>
-        </div>
-        
-        );
-
+  return (
+    <div>
+      <Updated name="thomas" />
+      <Updated name="Jerry" />
+    </div>
+  );
 }
+
+function Updated( {name} ){
+  return (
+    <div>
+          <img src="https://images.unsplash.com/photo-1547744152-14d985cb937f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZCUyMG11c3Rhbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+          <h1>Car: {name} </h1>
+          <p>
+            Ford Mustang Shelby GT 500
+          </p>
+    </div>
+  );
+}
+
+
 
 export default App;
